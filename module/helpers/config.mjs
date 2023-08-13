3663857004082160746211884ef1eb0b19d22737
scardrail <thibault.perquis@viacesi.fr>
2023-08-13 22:11:22 +0200

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const HAKAIKOUSEN = {};
+
+// Define constants here, such as:
+HAKAIKOUSEN.foobar = {
+  'bas': 'HAKAIKOUSEN.bas',
+  'bar': 'HAKAIKOUSEN.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ HAKAIKOUSEN.abilities = {
+  "str": "HAKAIKOUSEN.AbilityStr",
+  "dex": "HAKAIKOUSEN.AbilityDex",
+  "con": "HAKAIKOUSEN.AbilityCon",
+  "int": "HAKAIKOUSEN.AbilityInt",
+  "wis": "HAKAIKOUSEN.AbilityWis",
+  "cha": "HAKAIKOUSEN.AbilityCha"
+};
+
+HAKAIKOUSEN.abilityAbbreviations = {
+  "str": "HAKAIKOUSEN.AbilityStrAbbr",
+  "dex": "HAKAIKOUSEN.AbilityDexAbbr",
+  "con": "HAKAIKOUSEN.AbilityConAbbr",
+  "int": "HAKAIKOUSEN.AbilityIntAbbr",
+  "wis": "HAKAIKOUSEN.AbilityWisAbbr",
+  "cha": "HAKAIKOUSEN.AbilityChaAbbr"
+};
