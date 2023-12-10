5762b925a91206a9c26d2a098db1da06ae092100
scardrail <thibault.perquis@viacesi.fr>
2023-12-10 23:35:53 +0100

    replave name of system

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple hakaikousen System
+
+A simple game system for Foundry VTT which allows for flexible definition of Actors and Items to assist with hakaikousen or for running games which do not have a more complete system implementation available.
--- module/actor.js
+++ module/actor.js
@@ -28,7 +28,7 @@ export class SimpleActor extends Actor {
    * @type {boolean}
    */
   get isTemplate() {
-    return !!this.getFlag("worldbuilding", "isTemplate");
+    return !!this.getFlag("hakaikousen", "isTemplate");
   }
 
   /* -------------------------------------------- */
@@ -40,7 +40,7 @@ export class SimpleActor extends Actor {
 
     // Copy the actor's system data
     const data = this.toObject(false).system;
-    const shorthand = game.settings.get("worldbuilding", "macroShorthand");
+    const shorthand = game.settings.get("hakaikousen", "macroShorthand");
     const formulaAttributes = [];
     const itemAttributes = [];
 
